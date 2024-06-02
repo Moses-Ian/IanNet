42429a9f0631a0f5d75f4a575eee3418d160052c
Ian Moses <imoses2@hotmail.com>
2024-06-02 11:17:32 -0700

    feed forward is working

--- Perceptron.cs
+++ Perceptron.cs
@@ -12,11 +12,18 @@ namespace IanNet
     {
         public Context context;
         public Accelerator device;
-        float[] weights = new float[2];
+        public float[] weights = new float[2];
         public Action<Index1D, ArrayView1D<float, Stride1D.Dense>> fillRandomKernel;
+        public Action<
+            Index1D, 
+            ArrayView1D<float, Stride1D.Dense>, 
+            ArrayView1D<float, Stride1D.Dense>, 
+            ArrayView1D<float, Stride1D.Dense>> forwardKernel;
         
         // the memory on the gpu
         private MemoryBuffer1D<float, Stride1D.Dense> weightsBuffer;
+        private MemoryBuffer1D<float, Stride1D.Dense> inputsBuffer;
+        private MemoryBuffer1D<float, Stride1D.Dense> outputsBuffer;
 
         public Perceptron()
         {
@@ -27,12 +34,7 @@ namespace IanNet
 
             // get the data from the gpu
             weights = weightsBuffer.GetAsArray1D();
-
-            // log the result
-            Console.WriteLine(weights[0]);
-            Console.WriteLine(weights[1]);
         }
-
 
         public void InitGPU(bool forceCPU = false)
         {
@@ -44,11 +46,45 @@ namespace IanNet
             // there's a way to pick manually
             device = context.GetPreferredDevice(forceCPU).CreateAccelerator(context);
 
-            // convert our function into a kernel
+            // convert our functions into kernels
             fillRandomKernel = device.LoadAutoGroupedStreamKernel<Index1D, ArrayView1D<float, Stride1D.Dense>>(FillRandom);
+            forwardKernel = device.LoadAutoGroupedStreamKernel<
+                Index1D, 
+                ArrayView1D<float, Stride1D.Dense>, 
+                ArrayView1D<float, Stride1D.Dense>, 
+                ArrayView1D<float, Stride1D.Dense>>(forward);
 
             // allocate memory on the gpu
             weightsBuffer = device.Allocate1D<float>(weights.Length);
+        }
+
+        public float Forward(float[] inputs)
+        {
+            if (inputs.Length != weights.Length)
+                throw new Exception("Input length does not match weights length");
+
+            // copies the inputs to the gpu
+            inputsBuffer = device.Allocate1D<float>(inputs);
+
+            // allocates a size 1 float array for the output
+            outputsBuffer = device.Allocate1D<float>(1);
+
+            // run the kernel
+            forwardKernel(weights.Length, inputsBuffer, weightsBuffer, outputsBuffer);
+
+            // read the results from the gpu
+            float[] outputs = outputsBuffer.GetAsArray1D();
+            float output = outputs[0];
+
+            return Activation(output);
+        }
+
+        public float Activation(float n)
+        {
+            if (n < 0)
+                return -1;
+            else
+                return 1;
         }
 
         private static void FillRandom(Index1D node, ArrayView1D<float, Stride1D.Dense> output)
@@ -57,8 +93,16 @@ namespace IanNet
             // seed it with the index (but not 0)
             var random = new XorShift64Star((ulong)node+1);
 
-            // Generate a random number between 0 and 1
-            output[node] = random.NextFloat();
+            // Generate a random number between -1 and 1
+            output[node] = random.NextFloat() * 2 - 1;
+        }
+
+        private static void forward(Index1D node, ArrayView1D<float, Stride1D.Dense> inputs, ArrayView1D<float, Stride1D.Dense> weights, ArrayView1D<float, Stride1D.Dense> output)
+        {
+            float sum = 0;
+            for (var i = 0; i < inputs.Length; i++)
+                sum += inputs[i] * weights[i];
+            output[node] = sum;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ﻿## References
 
 https://www.youtube.com/watch?v=ow2o0xIVePE
+https://www.youtube.com/watch?v=ntKn5TPHHAk&list=PLRqwX-V7Uu6aCibgK1PTWWu9by6XFdCfh&index=2
