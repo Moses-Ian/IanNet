09922fb36218431f19c8d1c39bf22a5e633bd746
Ian Moses <imoses2@hotmail.com>
2024-07-20 18:47:34 -0700

    Starting the handwriting project

---
+++ IanNet/Net.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IanNet.IanNet
+{
+    public class Net
+    {
+        public Net()
+        {
+
+        }
+
+        public string ToString()
+        {
+            return "No input layers, no output layers";
+        }
+    }
+}
